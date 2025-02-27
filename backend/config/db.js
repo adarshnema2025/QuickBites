c22f43c0ac3da6b61aee823f136973ee6a608ba0
adarshnema2025 <adarshnema2025@gmail.com>
2025-02-27 22:40:36 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,20 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () =>{
+//     await mongoose.connect('mongodb+srv://dulanjalisenarathna93:E2JUb0zfaT2FVp8D@cluster0.exkxkun.mongodb.net/reactjs-food-delivery-app').then(()=>{
+//        console.log('DB connected') ;
+//     })
+// }
+
+
 import mongoose from "mongoose";
 
-export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://dulanjalisenarathna93:E2JUb0zfaT2FVp8D@cluster0.exkxkun.mongodb.net/reactjs-food-delivery-app').then(()=>{
-       console.log('DB connected') ;
-    })
+export const connectDB = async () => {
+    await mongoose.connect('mongodb://localhost:27017/fooditem')
+        .then(() => {
+            console.log('✅ DB connected to fooditem');
+        })
+        .catch((err) => {
+            console.error('❌ DB connection failed:', err.message);
+        });
 }
